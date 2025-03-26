eaff48c7fcab2c228d01926171a9418a18cd9553
pessima_au <albertpessima@outlook.com>
2025-03-26 13:11:06 +0000

    js error correction

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -49,7 +49,6 @@ async function getCompanies() {
     const response = await fetch("./data/members.json");
     const data = await response.json();
     displayCompanies(data);
-    console.log(data);
   } catch (error) {
     console.error("Error loading member data:", error);
   }
